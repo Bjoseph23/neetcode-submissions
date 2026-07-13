5caa037a4ef2938b30a04ede1ad2ca2a00e9254d
Bill Mageni <162094919+Bjoseph23@users.noreply.github.com>
2026-07-13 23:38:00 +0300

    Add: is-palindrome - submission-2

---
+++ Data Structures & Algorithms/is-palindrome/submission-2.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        cleaned_text =[]
+        for char in s:
+            if char.isalnum():
+                cleaned_text+= char.lower()
+
+        l=0
+        r=len(cleaned_text) -1
+        while l <= r:
+            if cleaned_text[l] != cleaned_text[r]:
+                return False
+            else:
+                l += 1
+                r -= 1
+        return True
