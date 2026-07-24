a79eb947cbc52fafe358d88ae2ed4e334b0e1f6f
Bill Mageni <162094919+Bjoseph23@users.noreply.github.com>
2026-07-24 13:02:03 +0300

    Add: two-integer-sum-ii - submission-2

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-2.py
@@ -0,0 +1,16 @@
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        map = defaultdict(int)
+        for i in range(len(numbers)):
+            comp = target - numbers[i]
+            if comp in map.keys():
+                print(comp)
+                if i+1 < map[comp]+1:
+                    return [i+1, map[comp]+1]
+                else:
+                    return[map[comp]+1, i+1]
+
+            map[numbers[i]] = i
+        #Time -> O(n)
+        #Space -> O(n) ❌
+        
